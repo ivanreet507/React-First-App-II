8f9bce4d93134eb946ca9b322242a95afda0c36c
ivanreet507 <ivkaur@calpoly.edu>
2023-10-01 18:06:07 -0700

    commit 2

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,26 +1,12 @@
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from './Form';
 
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
+
   ]);
   
   function removeOneCharacter (index) {
@@ -32,9 +18,10 @@ function MyApp() {
   
   return (
     <div className="container">
-      <Table characterData={characters} 
-	        removeCharacter={removeOneCharacter} />
-    </div>
+    <Table characterData={characters} 
+	    removeCharacter={removeOneCharacter} />
+    <Form />
+  </div>
   );
 }
 
